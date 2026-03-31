e07c182abfc97b68bfa5c5567a9c83c2d34e6e1b
YORKTEL\msteinbruck <steinbruckalex@gmail.com>
2026-03-31 13:23:00 -0400

    add sentry release workflow

---
+++ README.md
@@ -0,0 +1,1 @@
+Code to test error tracking with Sentry tool & github integration.
---
+++ code/errors.py
@@ -0,0 +1,17 @@
+import sentry_sdk
+import subprocess
+
+# Gets the git HEAD commit hash to use as the release identifier. 
+def get_release():
+    return subprocess.check_output(
+        ["git", "rev-parse", "HEAD"]
+    ).decode().strip()
+
+# Initialize Sentry
+sentry_sdk.init(
+    dsn="https://c97008fa69ad7303f784aa302fc5be41@o4505644778979328.ingest.us.sentry.io/4511140175085568",
+    release="demo-release-1"
+)
+
+# Force an error
+1 / 0
